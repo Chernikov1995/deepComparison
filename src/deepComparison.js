cb6ba6c3dded69c66a1443af14cb173e541425b0
Evgeny Chernikov <chernikovevge@gmail.com>
2020-10-20 22:55:21 +0300

    '+ recursion'

--- src/deepComparison.js
+++ src/deepComparison.js
@@ -8,30 +8,26 @@ function deepEqual (obj1, obj2) {
     return true
   }
 
-  if (obj1 === null || obj2 === null) {
-    return false
+  if (Number.isNaN(obj1) || Number.isNaN(obj2)) {
+    return Number.isNaN(obj1) && Number.isNaN(obj2)
   }
 
-  if (Number.isNaN(obj1) && Number.isNaN(obj2)) {
-    return true
+  if (!obj1 || !obj2) {
+    return false
   }
 
   if (typeof obj1 !== 'object' || typeof obj2 !== 'object') {
     return false
   }
 
-  if (obj1 && obj2 !== null) {
-    const object1Keys = Reflect.ownKeys(obj1)
-    const object2Keys = Reflect.ownKeys(obj2)
+  const object1Keys = Reflect.ownKeys(obj1)
+  const object2Keys = Reflect.ownKeys(obj2)
 
-    if (object1Keys.length !== object2Keys.length) {
-      return false
-    }
-
-    return object1Keys.every((key) => obj1[key] === obj2[key])
+  if (object1Keys.length !== object2Keys.length) {
+    return false
   }
 
-  return false
+  return object1Keys.every((key) => deepEqual(obj1[key], obj2[key]))
 }
 
 module.exports = deepEqual
