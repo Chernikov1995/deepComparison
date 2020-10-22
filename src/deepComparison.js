d3e8068b9deba525e5d96869260531a3a6fb99eb
Evgeny Chernikov <chernikovevge@gmail.com>
2020-10-22 23:38:36 +0300

    second try

--- src/deepComparison.js
+++ src/deepComparison.js
@@ -1,33 +1,85 @@
 
-/**
- * @param obj1
- * @param obj2
- */
-function deepEqual (obj1, obj2) {
-  if (obj1 === obj2) {
-    return true
-  }
+const primitiveTypes = [
+  'boolean',
+  'undefined',
+  'number',
+  'string',
+  'bigint',
+  'symbol'
+]
 
+/**
+ * @param value
+ */
+function checkPrimitiveType (value) {
+  return primitiveTypes.includes(typeof value)
+}
+
+const cache1 = new Map()
+const cache2 = new Map()
+
+function deepEqual (obj1, obj2) {
   if (Number.isNaN(obj1) || Number.isNaN(obj2)) {
     return Number.isNaN(obj1) && Number.isNaN(obj2)
   }
 
-  if (!obj1 || !obj2) {
-    return false
+  if (obj1 === null || obj2 === null) {
+    return obj1 === obj2
   }
 
-  if (typeof obj1 !== 'object' || typeof obj2 !== 'object') {
-    return false
+  if (checkPrimitiveType(obj1) && checkPrimitiveType(obj2)) {
+    return obj1 === obj2
   }
 
-  const object1Keys = Reflect.ownKeys(obj1)
-  const object2Keys = Reflect.ownKeys(obj2)
+  if (Object.getPrototypeOf(obj1) === Object.getPrototypeOf(obj2)) {
+    if (typeof obj1 === 'function') {
+      return obj1 === obj2
+    }
 
-  if (object1Keys.length !== object2Keys.length) {
-    return false
+    const check1 = cache1.get(obj1)
+    const check2 = cache2.get(obj2)
+
+    if (!check1) {
+      cache1.set(obj1, cache1.size + 1)
+    }
+
+    if (!check2) {
+      cache2.set(obj2, cache2.size + 1)
+    }
+
+    if (check1 && check2) {
+      return check1 === check2
+    }
+
+    if (cache1.size !== cache2.size) {
+      return false
+    }
+
+    const object1Keys = Reflect.ownKeys(obj1)
+    const object2Keys = Reflect.ownKeys(obj2)
+
+    if (object1Keys.length !== object2Keys.length) {
+      return false
+    }
+
+    return object1Keys.every((key) => deepEqual(obj1[key], obj2[key]))
   }
 
-  return object1Keys.every((key) => deepEqual(obj1[key], obj2[key]))
+  return false
 }
 
-module.exports = deepEqual
+/**
+ * @param obj1
+ * @param obj2
+ */
+function wrapper (obj1, obj2) {
+  cache1.clear()
+  cache2.clear()
+  return deepEqual(obj1, obj2)
+}
+
+module.exports = {
+  checkPrimitiveType,
+  wrapper,
+  deepEqual
+}
